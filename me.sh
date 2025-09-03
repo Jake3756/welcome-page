9696e5f62a0c29c63d5ce3a569882b1411ae2691
Jake3756 <jakeothmane4@gmail.com>
2025-09-03 22:08:37 +0000

    Add files via upload

---
+++ me.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "\                /           |        "
+echo " \      /\      /   _____    |    ____   ______                   _____"
+echo "  \    /  \    /   |     |   |   |      |      |    /\    /\     |     |"
+echo "   \  /    \  /    |_____|   |   |      |      |   /  \  /  \    |_____| "
+echo "    \/      \/     |_____    |   |____  |______|  /    \/    \   |_____ "
+
+#you need to download an audio_file then put it in the Music directory 
+
+
+mpg123 /home/$USER/Music/{audio_file}
